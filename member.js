dee8553e4163f5b0772b9c35e237fabf1bc9270d
Luis Mata <12718746+techguydev@users.noreply.github.com>
2023-12-23 21:07:54 +0000

    Copilot second commit

---
+++ member.js
@@ -0,0 +1,9 @@
+function skillsMember() {
+    var skills = ["HTML", "CSS", "JS", "React"];
+    var member = {
+        name: "John",
+        age: 25,
+        skills: skills,
+    };
+    return member.skills;
+}
